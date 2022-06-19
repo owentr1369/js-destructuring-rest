6bd45116320ef414474ab693f62a79187d2e04ed
owentr1369 <tamvaa1306@gmail.com>
2022-06-19 11:20:46 +0700

    destructuring

---
+++ script.js
@@ -0,0 +1,14 @@
+let array = ["JavaScript", "PHP", "Ruby"];
+
+// let a = array[0];
+// let b = array[1];
+// let c = array[2];
+
+// console.log(a, b, c);
+// Cach bth
+
+let [a, b, c] = array;
+console.log(a, b, c);
+let [d, , e] = array;
+console.log(d, e);
+// Destructuring
